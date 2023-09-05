6887f86045bb7bf139c9b38476fcff004a87d5a1
Rajesh <rajeshsharma989967@gmail.com>
2023-09-05 20:26:37 +0530

    second Commit

--- src/constants.js
+++ src/constants.js
@@ -36,7 +36,7 @@ export const query = [
 ];
 export const breakPoints = [
   { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: true },
   { width: 850, itemsToShow: 3 },
   { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
   { width: 1450, itemsToShow: 5 },
